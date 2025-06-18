4c82a1a1f6390503054a7652447a4571049ce54f
KeDeX <albers.mark02@gmail.com>
2025-06-18 15:14:08 +0200

    add models

---
+++ models/userEntry.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Linq;
+
+namespace DeskTreadmillLogger.models
+{
+    internal class UserEntry
+    {
+        public int id { get; set; }                 
+        public string name { get; set; }            
+        public double weightKg { get; set; }        
+        public int heightCm { get; set; }           
+        public DateTime birthdate { get; set; }     
+        public string gender { get; set; }          
+        public string email { get; set; }           
+        public DateTime createdAt { get; set; } = DateTime.UtcNow;
+        public ICollection<ActivityLogEntry> workoutSessions { get; set; }
+        public bool active { get; set; }
+
+        public UserEntry(int id, string name, double weightKg, int heightCm)
+        {
+            this.id = id;
+            this.name = name;
+            this.weightKg = weightKg;
+            this.heightCm = heightCm;
+            this.createdAt = DateTime.UtcNow;
+        }
+
+        public UserEntry() { }
+
+    }
+}
