3d26c2e9b4750b1e237c50875460e3ea255eeab9
KeDeX <albers.mark02@gmail.com>
2025-06-18 15:14:15 +0200

    add utility

---
+++ utility/JSONReader.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace DeskTreadmillLogger.utility
+{
+    internal class JSONReader
+    {
+        public static List<T> Load<T>(string filePath)
+        {
+            if (!File.Exists(filePath))
+                return new List<T>();
+
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<T>>(json);
+        }
+        // add a check if path exists
+        public static void Save<T>(List<T> data, string filePath)
+        {
+            var json = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, json);
+        }
+        // also make this method to remove a specific object in the json
+        public static void Remove<T>(List<T> data, string filePath)
+        {
+            var json = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, json);
+        }
+        //
+    }
+}
---
+++ utility/user.cs
@@ -0,0 +1,66 @@
+﻿using DeskTreadmillLogger.models;
+using System.IO;
+using System.Windows.Navigation;
+
+namespace DeskTreadmillLogger.utility
+{
+    internal class User
+    {
+        private static string userDataPath = "config/userData.json";
+        public static UserEntry currentUser;
+        private static List<UserEntry> usersList = new();
+
+        public static void Init()
+        {
+            if (!File.Exists(userDataPath))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(userDataPath));
+                File.WriteAllText(userDataPath, "[]");
+            }
+
+            usersList = JSONReader.Load<UserEntry>(userDataPath);
+        }
+
+        public static void CreateUser(string name, double weightKg, int heightCm)
+        {
+            int nextId = usersList.Any() ? usersList.Max(u => u.id) + 1 : 1;
+
+            currentUser = new UserEntry
+            {
+                id = nextId,
+                name = name,
+                weightKg = weightKg,
+                heightCm = heightCm
+            };
+
+            usersList.Add(currentUser);
+            JSONReader.Save(usersList, userDataPath);
+        }
+
+        public static void DeleteUser(int id)
+        {
+            SelectUser(id);
+            usersList.Remove(currentUser);
+            JSONReader.Remove(currentUser, userDataPath);
+        }
+
+        public static void SelectUser(int id)
+        {
+            var temp = usersList.FirstOrDefault(u => u.id == id);
+            if (temp == null)
+            {
+                return;
+            }
+            else
+            {
+                currentUser = temp;
+            }
+        }
+
+        public static List<UserEntry> GetAllUsers()
+        {
+            return usersList;
+        }
+
+    }
+}
