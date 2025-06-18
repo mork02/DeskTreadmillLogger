b44d5a7df051d7e56b6d8ae72f0836bd8177ad6a
KeDeX <albers.mark02@gmail.com>
2025-06-19 00:57:24 +0200

    change values

--- models/ActivityLogEntry.cs
+++ models/ActivityLogEntry.cs
@@ -3,12 +3,12 @@
     internal class ActivityLogEntry
     {
         public int id { get; set; }
-        public int userId { get; set; }
         public UserEntry user { get; set; }
         public DateTime timestamp { get; set; } = DateTime.UtcNow;
-        public double distanceKm { get; set; }  
+        public double speed { get; set; }
         public TimeSpan duration { get; set; }  
-        public double caloriesBurned { get; set; }
+        public double distanceKm { get; set; }  
+        public int caloriesBurned { get; set; }
         public string notes { get; set; }
     }
 }
